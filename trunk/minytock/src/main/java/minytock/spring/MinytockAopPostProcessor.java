b4b08eac58f30865cff7041c6a0e856dbd51d74f
reesbyars <reesbyars@3a1f1a0c-5181-5bd5-e00e-0f584b96c05c>
2013-01-15 18:28:47 +0000

    refactored the spring post-processors to be a little cleaner and have better logging

--- trunk/minytock/src/main/java/minytock/spring/MinytockAopPostProcessor.java
+++ trunk/minytock/src/main/java/minytock/spring/MinytockAopPostProcessor.java
@@ -1,6 +1,7 @@
 package minytock.spring;
 
 import minytock.Minytock;
+import minytock.delegate.DelegationException;
 import minytock.delegate.DelegationHandlerCache;
 import minytock.spy.Spy;
 
@@ -8,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.aop.target.HotSwappableTargetSource;
-import org.springframework.beans.BeansException;
 
 /**
  * Pretty the same as the {@link MinytockPostProcessor}, but made to work with Spring AOP proxies.
@@ -22,38 +22,30 @@ public class MinytockAopPostProcessor extends MinytockPostProcessor {
     public MinytockAopPostProcessor() {
     	Minytock.provider = new SpringAopDelegationHandlerProvider(Spy.get(DelegationHandlerCache.class).from(Minytock.provider));
     }
-
+    
     @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-    	LOG.info("Minytock checking [" + beanName +  "] for delegation");
-    	boolean doProxy = false;
-    	Class<?> realClass = AopUtils.getTargetClass(bean);
-        for (String pack : mockablePackages) {
-            if (realClass.getName().startsWith(pack) && !beanName.endsWith("Test")) {
-                doProxy = true;
-                break;
-            }
-        }
-        Object minytockProxy = bean;
-        try {
-            if (doProxy) {
-            	LOG.info("Minytock preparing [" + beanName +  "] for delegation");
-                minytockProxy = Minytock.provider.getHandler(minytockProxy, realClass, false).getProxy();
-            } else {
-            	LOG.info("Minytock skipping [" + beanName +  "] as not eligible delegation");
-            }
-        } catch (Exception e) {
-            //nuthin
-        }
+    protected Class<?> getBeanClass(Object bean) {
+    	return AopUtils.getTargetClass(bean);
+    }
+    
+    @Override
+    protected Object prepare(Object bean, Class<?> beanClass) throws DelegationException {
+    	
+    	Object minytockProxy = Minytock.provider.getHandler(bean, beanClass, false).getProxy();
+        
+        //simply swap if bean is hot-swappable
         if (bean instanceof HotSwappableTargetSource ) {
+        	
         	LOG.info("Bean instance of HotSwappableTargetSource, setting Minytock proxy as the AOP proxy target.");
         	((HotSwappableTargetSource) bean).swap(minytockProxy);
         	return bean;
+        	
         } else {
+        	
         	LOG.info("Bean not an instance of HotSwappableTargetSource, returning the Minytock proxy as the bean.");
         	return minytockProxy;
+        	
         }
-        
     }
     
 }
