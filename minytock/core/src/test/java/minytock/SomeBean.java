c7a646d0e4a7376feee5a382ecdff379fd5de28e
reesbyars <reesbyars@3a1f1a0c-5181-5bd5-e00e-0f584b96c05c>
2013-01-15 21:30:03 +0000

    re-deleting some ghost files...

--- minytock/core/src/test/java/minytock/SomeBean.java
+++
@@ -1,5 +0,0 @@
-package minytock;
-
-public class SomeBean {
-
-}
--- minytock/core/src/test/java/minytock/TestBean.java
+++
@@ -1,9 +0,0 @@
-package minytock;
-
-public class TestBean {
-	protected String field = "original";
-	public TestBean(){}
-	public void test(String msg) {
-		System.out.println(msg);
-	}
-}
