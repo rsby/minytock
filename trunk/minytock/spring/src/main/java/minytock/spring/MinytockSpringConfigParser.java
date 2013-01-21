abc5627f05d8b1e44b49abc2a91d5d2613efe56b
reesbyars <reesbyars@3a1f1a0c-5181-5bd5-e00e-0f584b96c05c>
2013-01-21 22:34:50 +0000

    put the pieces in place for adding spring namespace support for easier xml config.

---
+++ trunk/minytock/spring/src/main/java/minytock/spring/MinytockSpringConfigParser.java
@@ -0,0 +1,16 @@
+package minytock.spring;
+
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+public class MinytockSpringConfigParser extends AbstractBeanDefinitionParser {
+
+	@Override
+	protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
+		
+		return null;
+	}
+
+}
