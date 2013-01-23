559c32ee31b76acf2fb85fb88dbf0159f130a7aa
reesbyars <reesbyars@3a1f1a0c-5181-5bd5-e00e-0f584b96c05c>
2013-01-23 15:55:47 +0000

    tuned the configuration parameters a bit

--- trunk/minytock/minytock-maven-plugin/src/main/java/minytock/maven/MinytockMojo.java
+++ trunk/minytock/minytock-maven-plugin/src/main/java/minytock/maven/MinytockMojo.java
@@ -13,8 +13,14 @@ import org.apache.maven.project.MavenProject;
 @Mojo(name="minytock", defaultPhase=LifecyclePhase.PREPARE_PACKAGE)
 public class MinytockMojo extends AbstractMojo {
 	
-	@Parameter(property = "minytock", defaultValue="false")
-	private String executionFlag;
+	@Parameter(property = "minytock.deploy", defaultValue = "false")
+	private boolean deploy;
+	
+	@Parameter(property = "minytock.addJars", defaultValue = "true")
+	private boolean addJars;
+	
+	@Parameter(property = "minytock.version", defaultValue = "unspecified")
+	private String version;
 	
 	@Component
     private MavenProject project;
@@ -22,17 +28,33 @@ public class MinytockMojo extends AbstractMojo {
 	@Component
     private ArtifactFactory artifactFactory;  
 	
-    @SuppressWarnings("unchecked")
+    
 	public void execute() throws MojoExecutionException {
-    	if ("true".equals(executionFlag)) {
-    		getLog().info( "executing minytock mojo" );
-    		String version = getClass().getPackage().getImplementationVersion();
-    		getLog().info("minytock version - " + version);
-            Artifact artifact = artifactFactory.createArtifact("com.googlecode.minytock", "minytock-core", version, "compile", "jar");
-            this.project.getDependencyArtifacts().add(artifact);
+    	if (deploy) {
+    		getLog().info( "executing minytock mojo");
+    		
+    		this.resolveVersion();
+    		this.addJars();
+            
             getLog().info( "completed minytock mojo" );
     	} else {
     		getLog().info( "skipping minytock mojo" );
     	}
+    }
+	
+	protected void resolveVersion() {
+		if ("unspecified".equals(version)) {
+			version = getClass().getPackage().getImplementationVersion();
+		}
+		getLog().info("minytock version - " + version);
+	}
+    
+	@SuppressWarnings("unchecked")
+    protected void addJars() {
+		if (addJars) {
+			getLog().info( "adding jars..." );
+			Artifact artifact = artifactFactory.createArtifact("com.googlecode.minytock", "minytock-core", version, "compile", "jar");
+	        this.project.getDependencyArtifacts().add(artifact);
+		}
     }
 }
