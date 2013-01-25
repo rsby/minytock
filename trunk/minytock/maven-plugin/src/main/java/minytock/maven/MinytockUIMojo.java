3e44f8cdbfb876ec5a71e994b8d46803cbcaabcd
reesbyars <reesbyars@3a1f1a0c-5181-5bd5-e00e-0f584b96c05c>
2013-01-25 02:37:46 +0000

    implemented a mojo for auto-deploy of the ui

--- trunk/minytock/maven-plugin/src/main/java/minytock/maven/MinytockUIMojo.java
+++ trunk/minytock/maven-plugin/src/main/java/minytock/maven/MinytockUIMojo.java
@@ -1,13 +1,21 @@
 package minytock.maven;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
- * 
- * TODO allow a user to specify additional jars, wars
  * 
  * @author reesbyars
  *
@@ -15,11 +23,42 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo(name="deploy-ui", defaultPhase=LifecyclePhase.PREPARE_PACKAGE)
 public class MinytockUIMojo extends AbstractMinytockMojo {
 	
+	@Parameter(property = "minytock.autoDeployDirectory")
+	private String autoDeployDirectory;
+	
 	@Component
-    private ArtifactFactory artifactFactory;  
+    private ArtifactFactory artifactFactory; 
+	
+	@Component
+    protected ArtifactResolver artifactResolver;
+
+	@Parameter(property = "project.remoteArtifactRepositories", readonly=true, required=true)
+    protected List<?> remoteRepositories;
+
+	@Parameter(property = "localRepository", readonly=true, required=true)
+    protected ArtifactRepository localRepository;
     
 	public void doMojo() throws Exception {
-    	
+		
+		if (autoDeployDirectory != null) {
+			
+			Artifact war = artifactFactory.createArtifact("com.googlecode.minytock", "minytock-ui", version, "compile", "jar");
+
+			artifactResolver.resolve(war, remoteRepositories, localRepository);
+			
+			FileInputStream fis = null;
+			fis = new FileInputStream(war.getFile());
+			
+			File destination = new File(autoDeployDirectory + "/minytock-ui.jar");
+			FileOutputStream fos = new FileOutputStream(destination);
+			
+			IOUtils.copy(fis, fos);
+			
+			IOUtils.closeQuietly(fis);
+			IOUtils.closeQuietly(fos);
+			
+		}
+		
     }
 	
 }
